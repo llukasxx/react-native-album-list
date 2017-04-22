cf97267b659fcd97d935107ab23ba141cb77a88c
Łukasz Gorzelany <llukasxx@gmail.com>
2017-04-22 14:11:46 +0200

    Initial commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+export default class AlbumList extends Component {
+  state = {
+    albums: []
+  };
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((response) => {
+        this.setState({ albums: response.data });
+      });
+  }
+  renderAlbums() {
+    const { albums } = this.state;
+    return albums.map(album => <AlbumDetail key={album.title} album={album} />);
+  }
+  render() {
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
