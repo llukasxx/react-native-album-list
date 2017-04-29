fcc22a0c2c2e464260c2766076a2c30a744eaa7a
Łukasz Gorzelany <llukasxx@gmail.com>
2017-04-29 15:58:07 +0200

    Add pretty album list renderer

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,8 @@ import { View } from 'react-native';
 
 const Card = (props) => {
   return (
-    <View>
-
+    <View style={styles.containerStyle}>
+      {props.children}
     </View>
   );
 };
@@ -18,7 +18,11 @@ const styles = {
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
-    shadowRadius: 2
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10
   }
 };
 
