9d3df5ee1cdec6554b5c466973a6eb74d213e66a
Łukasz Gorzelany <llukasxx@gmail.com>
2017-04-30 12:36:43 +0200

    Add buy button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = () => {
+const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles;
 
   return (
-    <TouchableOpacity onPress={() => console.log('press')} style={buttonStyle}>
+    <TouchableOpacity
+      onPress={onPress}
+      style={buttonStyle}
+    >
       <Text style={textStyle}>
-        Clik
+        {children}
       </Text>
     </TouchableOpacity>
   );
